f600d8fbf0977007b3687ed6471ab99eb1445cc8
MuhammadRiswanBadali <riswanbadali9@gmail.com>
2024-11-21 09:16:24 +0800

    menambahkan code untuk menghubungkan aplikasi dengan database SQLite

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,3 +1,8 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +13,12 @@
  * @author USER
  */
 public class DatabaseConnection {
-    
+     public static Connection connect() {
+        try {
+            return DriverManager.getConnection("jdbc:sqlite:keuangan.db");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
